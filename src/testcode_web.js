2a3dd99b306c0b1ceb47209c6c78c54b3bc67cfd
vicayo <vicayo.c@gmail.com>
2025-11-26 02:01:13 -0500

    update index.css

--- src/testcode_web.js
+++ src/testcode_web.js
@@ -8,38 +8,33 @@ import {
     utf8Decoder, utf8Encoder
 } from "./terminal_core";
 
-if (
-    parameters.length === 3 &&
-    parameters[0].length > 6 && parameters[0].startsWith('-ipp=') &&  // ip:port
-    parameters[1].length > 5 && parameters[1].startsWith('-key=')     // user key
-) {
-    const
-        ipp = parameters[0].substring(5),
-        userKey = parameters[1].substring(5);
-    if (parameters[2] === '-new') { // Command: mycloud -ipp=[ip:port] -key=[userKey] -new
-        try {
-            const [status, stream] = await fetch(
-                `http://${ipp}/mycloud/users/register/`,
-                {
-                    method: 'POST',
-                    body: formData({ // short enough so we can use JSON
-                        user_key: userKey
-                    })
-                }
-            ).then(
-                async (res) => [res.status, await res.json()]
-            );
-            if (status !== 200) {
-                const {error: error} = stream; // stream here is a json object
-                currentTerminalCore.printToWindow(`${error}`, RGBColor.red);
-                return;
+
+const
+    ipp = parameters[0].substring(5),
+    userKey = parameters[1].substring(5);
+if (parameters[2] === '-new') { // Command: mycloud -ipp=[ip:port] -key=[userKey] -new
+    try {
+        const [status, stream] = await fetch(
+            `http://${ipp}/mycloud/users/register/`,
+            {
+                method: 'POST',
+                body: formData({ // short enough so we can use JSON
+                    user_key: userKey
+                })
             }
-            currentTerminalCore.printToWindow(' --> Registered a user key.', RGBColor.green);
-        } catch (error) {
+        ).then(
+            async (res) => [res.status, await res.json()]
+        );
+        if (status !== 200) {
+            const {error: error} = stream; // stream here is a json object
             currentTerminalCore.printToWindow(`${error}`, RGBColor.red);
+            return;
         }
-        return;
+        currentTerminalCore.printToWindow(' --> Registered a user key.', RGBColor.green);
+    } catch (error) {
+        currentTerminalCore.printToWindow(`${error}`, RGBColor.red);
     }
+    return;
 }
 
 
